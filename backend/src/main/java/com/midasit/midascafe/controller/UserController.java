32057402d6d300530b0962e751d9531da78ca26e
sym0417 <sym0417@midasit.com>
2022-06-09 16:06:27 +0900

    * coffee 초기 데이터 등록

--- backend/src/main/java/com/midasit/midascafe/controller/UserController.java
+++ backend/src/main/java/com/midasit/midascafe/controller/UserController.java
@@ -6,6 +6,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.List;
 
 @RestController
@@ -23,5 +27,25 @@ public class UserController {
     public Integer GetUsers() {
         List<User> users = userRepository.findAll();
         return users.size();
+    }
+
+    @GetMapping("/users/test-set1")
+    public void SetUsers1() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("USER");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        User tempUser = new User();
+//        tempUser.setId(1);
+        tempUser.setName("bcd");
+
+        em.persist(tempUser);
+        tx.commit();
+
+        em.close();
+        emf.close();
     }
 }
