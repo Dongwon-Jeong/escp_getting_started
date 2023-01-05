752eb73b5306b4ba63921c92128c3038abc76e7e
kys0522 <kys0522@midasit.com>
2023-01-05 09:50:27 +0900

    * initial source

---
+++ backend_coffee_mcafe_api/src/main/java/com/midasit/midascafe/MidasCafeApplication.java
@@ -0,0 +1,13 @@
+package com.midasit.midascafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidasCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidasCafeApplication.class, args);
+    }
+
+}
