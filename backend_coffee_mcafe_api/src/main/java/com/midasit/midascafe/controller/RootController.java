4916daf39470d95dd87896d1aaabb7fd9960047f
jdw1227 <jdw1227@midasit.com>
2023-02-21 10:00:23 +0900

    [MWORKS-790] menu list를 uchef 서버에서 받아와 파싱하여 주게 변경

--- backend_coffee_mcafe_api/src/main/java/com/midasit/midascafe/controller/RootController.java
+++ backend_coffee_mcafe_api/src/main/java/com/midasit/midascafe/controller/RootController.java
@@ -1,16 +1,11 @@
 package com.midasit.midascafe.controller;
 
 import com.midasit.midascafe.controller.rqrs.PayOrderRq;
-import com.midasit.midascafe.dao.MenuDAO;
 import com.midasit.midascafe.dto.ResponseData;
 import com.midasit.midascafe.service.PayService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,44 +14,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-
 
 @Api(tags = "Root Controller")
 @RestController
 @RequiredArgsConstructor  // 생성자 주입
 public class RootController {
-    private final MenuDAO menuDAO;
     private final PayService payService;
     @Operation(summary = "졸음 방지용", description = "서버 졸음 방지")
     @GetMapping("/caffeine")
     public ResponseEntity<Void> caffeine() {
-        return ResponseEntity.ok(null);
-    }
-
-
-    // 메뉴 추가를 위한 임시 메서드
-    @Operation(summary = "메뉴 추가를 위한 임시 메서드", description = "메뉴 추가를 위한 임시 메서드")
-    @PostMapping("/menu/database/init")
-    public ResponseEntity<Void> initDb() throws IOException, ParseException {
-        //ClassPathResource initDbResource = new ClassPathResource("init_db.json");
-        JSONParser parser = new JSONParser();
-        Reader reader;
-        reader = new FileReader("src/main/resources/init_db.json");
-        JSONObject initDBJson = (JSONObject) parser.parse(reader);
-        JSONArray page = (JSONArray) ((JSONObject) initDBJson.get("PAGELIST")).get("PAGE");
-        for(int type = 0; type < page.size(); type++) {
-            JSONArray orderButtonComp = (JSONArray) ((JSONObject) ((JSONArray) ((JSONObject) ((JSONObject) page.get(type)).get("LISTCOMP")).get("LISTROW")).get(0)).get("ORDERBUTTONCOMP");
-            for(int menuIdx = 0; menuIdx < orderButtonComp.size(); menuIdx++) {
-                JSONObject menuObj = (JSONObject) orderButtonComp.get(menuIdx);
-                String name = (String) menuObj.get("menutitle");
-                String code = (String) menuObj.get("item_code");
-                int unitPrice = ((Long) menuObj.get("price")).intValue();
-                //menuDAO.registerMenu(name, code, unitPrice, type);
-            }
-        }
         return ResponseEntity.ok(null);
     }
 
