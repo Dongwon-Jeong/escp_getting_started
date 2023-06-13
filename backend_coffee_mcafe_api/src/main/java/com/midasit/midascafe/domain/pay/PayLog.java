ab218d98790e32e1a73fde550a58f070e234e096
jdw1227 <jdw1227@midasit.com>
2023-06-13 17:35:35 +0900

    fix.

--- backend_coffee_mcafe_api/src/main/java/com/midasit/midascafe/domain/pay/PayLog.java
+++ backend_coffee_mcafe_api/src/main/java/com/midasit/midascafe/domain/pay/PayLog.java
@@ -3,10 +3,7 @@ package com.midasit.midascafe.domain.pay;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
@@ -20,6 +17,7 @@ public class PayLog {
     LocalDateTime createdDateTime;
 
     @NotNull
+    @Column(length = 5000)
     String url;
 
     public PayLog(String url) {
